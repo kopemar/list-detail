163273fc4c546bed2b5910808abf514e8b304e12
Martina Kopecká <kopecm26@fel.cvut.cz>
2019-12-05 00:37:57 +0100

    HttpException catching

--- app/src/main/java/cz/kopemar/listdetail/repository/GithubRepository.kt
+++ app/src/main/java/cz/kopemar/listdetail/repository/GithubRepository.kt
@@ -1,14 +1,42 @@
 package cz.kopemar.listdetail.repository
 
+import android.util.Log
+import cz.kopemar.listdetail.model.Branch
+import cz.kopemar.listdetail.model.CommitWrapper
+import cz.kopemar.listdetail.model.Repository
 import cz.kopemar.listdetail.rest.GitHubService
+import retrofit2.HttpException
 
 class GithubRepository(private val githubApi: GitHubService) {
-    suspend fun getAllRepos() = githubApi.getAllRepos()
 
-    suspend fun getAllBranchesInRepo(repoName: String) = githubApi.getBranchesInRepo(repoName)
+    suspend fun getAllRepos(): List<Repository> {
+        return try {
+            githubApi.getAllRepos()
+        } catch (e: HttpException) {
+            Log.d(TAG, "Got HttpException ")
+            ArrayList()
+        }
+    }
 
-    suspend fun getAllCommitsInRepo(repoName: String) = githubApi.getAllCommitsInRepo(repoName)
+    suspend fun getAllBranchesInRepo(repoName: String): List<Branch> {
+        return try {
+            githubApi.getBranchesInRepo(repoName)
+        } catch (e: HttpException) {
+            Log.d(TAG, "Got HttpException ")
+            ArrayList()
+        }
+    }
 
-    suspend fun getCommitsInRepo(repoName: String, limit: Int) =
-        githubApi.getCommitsInRepo(repoName, limit)
+    suspend fun getCommitsInRepo(repoName: String, limit: Int): List<CommitWrapper> {
+        return try {
+            githubApi.getCommitsInRepo(repoName, limit)
+        } catch (e: HttpException) {
+            Log.d(TAG, "Got HttpException ")
+            ArrayList()
+        }
+    }
+
+    companion object {
+        const val TAG = "GithubRepository"
+    }
 }
